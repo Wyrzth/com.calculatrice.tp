e565a0ba6fb8cc701aa3c23da640946a8c579cc6
wyrzth <joe.cadieu@gmail.com>
2015-10-15 09:22:55 +0200

    add checkstyle validation

--- src/com/calculatrice/tp/java/controller/Controller.java
+++ src/com/calculatrice/tp/java/controller/Controller.java
@@ -3,15 +3,12 @@ package com.calculatrice.tp.java.controller;
 import com.calculatrice.tp.java.model.Calculator;
 
 public class Controller {
-	
-	public static void main(String[] args) {
-		
-		 
-		 TestCases test = new TestCases();
-		 test.testAddition();
-		 test.testMultiply();
-	
-		
-	}
+
+  public static void main(String[] args) {
+
+    TestCases test = new TestCases();
+    test.testAddition();
+    test.testMultiply();
+  }
 
 }
--- src/com/calculatrice/tp/java/model/Calculator.java
+++ src/com/calculatrice/tp/java/model/Calculator.java
@@ -1,27 +1,21 @@
 package com.calculatrice.tp.java.model;
 
 public class Calculator {
+  private double doubleA;
+  private double doubleB;
 
-    private double doubleA;
-	private double doubleB;
+  public Calculator(double doubleA, double doubleB) {
+    this.doubleA = doubleA;
+    this.doubleB = doubleB;
+  }
 
-	public Calculator(double doubleA, double doubleB) {
-	this.doubleA = doubleA;	
-	this.doubleB = doubleB;
-    }
+  public double addition() {
+    return this.doubleA + this.doubleB;
+  }
 
-	public double addition(){
-		return this.doubleA + this.doubleB;
-		
-	}
-	
-	public double multiply(){
-		return doubleA * doubleB;
-		
-	}
-	
-	
-	
-	
+  public double multiply() {
+    return doubleA * doubleB;
+
+  }
 
 }
